e25f9f11c24a6816c204b3e5a0c0afee334ff324
konno-yu <yuta.konno.socie@gmail.com>
2021-05-03 16:13:40 +0900

    クライアントコードの初期セットアップ

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+    return (
+        <button>hello all over the world</button>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
