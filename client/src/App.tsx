285b867aa75c958d7b0b822ba56853f191c421d3
konno-yu <yuta.konno.socie@gmail.com>
2021-05-04 11:09:07 +0900

    index.tsxからAppコンポーネントを切り分け

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <button>分離</button>
+    )
+}
+
+export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App: React.FC = () => {
-    return (
-        <button>hello all over the world</button>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    /**
+     * StrictModeにしておくと、
+     *  ● 安全でないライフサイクル
+     *  ● 文字列refAPIの使用
+     *  ● findDOMNodeの使用
+     *  ● 意図しない副作用
+     *  ● contextAPIの使用
+     * を検知して警告してくれる（※開発版ビルドのみ）
+     */
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
