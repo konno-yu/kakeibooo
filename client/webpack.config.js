92e555cf274fd03d09851f13c1bb4e1068a08f76
konno-yu <yuta.konno.socie@gmail.com>
2021-05-09 15:35:04 +0900

    .sassが利用できるようにライブラリを追加して設定を変更

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            exclude: /node_modules/
         }],
     },
     resolve: {
