da883ce16bbc41dc0e1709757f3fc05b4f3801ac
konno-yu <yuta.konno.socie@gmail.com>
2021-06-19 15:17:10 +0900

    食費入力部分のコンポーネント群を作成

---
+++ client/src/components/finance/ReceiptHeader.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import logo from '../../images/icon.svg';
+
+const ReceiptHeader: React.FC = () => {
+    return (
+        <div className="root--header">
+            <div className="title">Kakeibooo</div>
+            <div className="date">2021/05/23（日）</div>
+        </div>
+    )
+}
+
+export default ReceiptHeader;
