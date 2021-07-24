a1bb8b8aa863c7425895265c6d370dc1f8441dc4
konno-yu <yuta.konno.socie@gmail.com>
2021-07-24 11:49:09 +0900

    Drawerメニューのアカウントアイコンを変えてみる

--- client/src/components/drawer/DrawerSvgIcons.tsx
+++ client/src/components/drawer/DrawerSvgIcons.tsx
@@ -59,3 +59,22 @@ export const SettingIcon: React.FC<Drawer.SvgIconProps> = (props) => {
 
     )
 }
+
+export const FaceIcon: React.FC<Drawer.SvgIconProps> = (props) => {
+    return (
+        <>
+            <svg viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" width="80" height="80">
+                <mask id="mask__beam" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36">
+                    <rect width="36" height="36" rx="72" fill="white"></rect>
+                </mask>
+                <g mask="url(#mask__beam)">
+                    <rect width="36" height="36" fill="#424242"></rect>
+                    <rect x="0" y="0" width="36" height="36" transform="translate(7 1) rotate(53 18 18) scale(1.2)" fill="#ff5252" rx="6"></rect>
+                    <g transform="translate(3.5 -2) rotate(3 18 18)">
+                        <path d="M15 21c2 1 4 1 6 0" stroke="black" fill="none" stroke-linecap="round"></path>
+                        <rect x="11" y="14" width="1.5" height="2" rx="1" stroke="none" fill="black"></rect>
+                        <rect x="23" y="14" width="1.5" height="2" rx="1" stroke="none" fill="black"></rect>
+                    </g></g></svg>
+        </>
+    )
+}
