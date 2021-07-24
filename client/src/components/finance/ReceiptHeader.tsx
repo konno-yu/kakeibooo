87095319b974755c616385940851d6a6384c6134
konno-yu <yuta.konno.socie@gmail.com>
2021-07-24 11:51:40 +0900

    食費入力欄の日付がカレンダーの選択に追従するように修正

--- client/src/components/finance/ReceiptHeader.tsx
+++ client/src/components/finance/ReceiptHeader.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
-import logo from '../../images/icon.svg';
+import { financeContext, useFinance } from './FinanceContext';
+import * as dfns from 'date-fns';
 
 const ReceiptHeader: React.FC = () => {
+    const dayOfWeekLabel = ['日', '月', '火', '水', '木', '金', '土'];
+    const getDisplayYMDD = (targetDate: Date) => {
+        const zeroPadding = (val: number) => ('000' + val).slice(-2);
+        return [
+            dfns.getYear(targetDate),
+            zeroPadding(dfns.getMonth(targetDate) + 1),
+            zeroPadding(dfns.getDate(targetDate)),
+            dayOfWeekLabel[dfns.getDay(targetDate)]
+        ]
+    }
+    const context = React.useContext(financeContext);
+    const [dispYear, dispMonth, dispDate, dispDay] = getDisplayYMDD(context.targetDate);
+
     return (
         <div className="root--header">
             <div className="title">Kakeibooo</div>
-            <div className="date">2021/05/23（日）</div>
+            <div className="date">
+                {dispYear}/{dispMonth}/{dispDate}（{dispDay}）</div>
         </div>
     )
 }
