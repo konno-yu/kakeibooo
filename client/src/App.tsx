033426d07b72c3cb15e1cc76a11671003b444c24
konno-yu <yuta.konno.socie@gmail.com>
2021-08-16 11:04:35 +0900

    Appコンポーネントもstyled-components化
    Material-UIのスタイルを上書きするために StylesProvider を指定するようにした

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,41 @@
+import { StylesProvider } from '@material-ui/styles';
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import { Drawer } from './components/drawer/Drawer';
 import { FinanceView } from './view/FinanceView';
 
 export const App: React.FC = () => {
     return (
-        <Router>
-            <div className="drawer">
-                <Drawer />
-            </div>
-            <div className="content">
-                <Switch>
-                    <Route path="/home" />
-                    <Route path="/finance" component={FinanceView} />
-                    <Route path="/analyze" />
-                    <Route path="/fridge" />
-                    <Route path="/setting" />
-                </Switch>
-            </div>
-        </Router>
+        <StylesProvider injectFirst>
+            <Router>
+                <SC.Drawer>
+                    <Drawer />
+                </SC.Drawer>
+                <SC.Content>
+                    <Switch>
+                        <Route path="/home" />
+                        <Route path="/finance" component={FinanceView} />
+                        <Route path="/analyze" />
+                        <Route path="/fridge" />
+                        <Route path="/setting" />
+                    </Switch>
+                </SC.Content>
+            </Router>
+        </StylesProvider>
     )
 }
 
+const SC = {
+    Drawer: styled.div`
+        font-family: "M PLUS Rounded 1c", sans-serif;
+        height: 100vh;
+        width: 15%;
+        border-right: 1px solid #EEEEEE;
+    `,
+    Content: styled.div`
+        width: 85%;
+        height: 100vh;
+        background: #ECEFF1;
+    `
+};
