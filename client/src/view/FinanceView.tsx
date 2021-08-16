5313998410803b3d286adc7166446fa67ea5bafa
konno-yu <yuta.konno.socie@gmail.com>
2021-08-16 11:02:45 +0900

    家計簿画面の大元になるViewもstyled-components化

--- client/src/view/FinanceView.tsx
+++ client/src/view/FinanceView.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
-import '../style/finance.scss';
 import { Calendar } from '../components/finance/Calendar';
 import { Receipt } from '../components/finance/Receipt';
 import { financeContext, useFinance } from '../components/finance/FinanceContext';
+import styled from 'styled-components';
 
 export const FinanceView: React.FC = () => {
     const context = useFinance();
     return (
-        <div id="root--finance">
+        <SC.FinanceView>
             <financeContext.Provider value={context}>
                 <Calendar />
                 <Receipt />
             </financeContext.Provider>
-        </div>
+        </SC.FinanceView>
     )
 }
 
+const SC = {
+    FinanceView: styled.div`
+        height: calc(100vh - 24px);
+        padding: 12px;
+        display: flex;
+        justify-content: space-between;
+    `
+};
