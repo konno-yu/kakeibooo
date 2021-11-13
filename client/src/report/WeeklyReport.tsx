426cb3205bb8c55976f4a11df5917f553c766468
konno-yu <yuta.konno.socie@gmail.com>
2021-11-13 22:44:59 +0900

    週の食費をカードコンポーネントに分離

--- client/src/report/WeeklyReport.tsx
+++ client/src/report/WeeklyReport.tsx
@@ -1,21 +1,18 @@
-import { isSaturday, nextSaturday, nextSunday, sub } from "date-fns";
+import { isSaturday, nextSaturday, nextSunday, subDays } from "date-fns";
 import { useEffect, useState } from "react";
-import { AiOutlineCheckCircle } from "react-icons/ai";
 import styled from "styled-components";
-import { Button } from "@material-ui/core";
-import { TiChevronLeft, TiChevronRight } from "react-icons/ti";
-import { GiPayMoney } from 'react-icons/gi';
-import { WeeklyCosting } from "./WeeklyCosting";
+import { WeeklyTransition } from "./WeeklyTransition";
 import { MemoEditor } from "./MemoEditor";
 import * as ReceiptRest from '../rest/financeRest';
 import DailyReceiptModel from "../components/receipt/model/DailyReceiptModel";
 import ReceiptModel from "../components/receipt/model/ReceiptModel";
+import { WeeklyCostCard } from "./WeeklyCostCard";
 
 export const WeeklyReport: React.FC = () => {
     const setInitDuration = (): { from: Date, to: Date } => {
         const today = new Date().setHours(0, 0, 0);
         const saturday = isSaturday(today) ? today : nextSaturday(today);
-        const sunday = sub(saturday, { days: 6 });
+        const sunday = subDays(saturday, 6);
         return { from: sunday, to: new Date(saturday) };
     }
     const [duration, setDuration] = useState<{ from: Date, to: Date }>(setInitDuration());
@@ -35,7 +32,7 @@ export const WeeklyReport: React.FC = () => {
     }, [duration])
 
     const prevWeek = () => {
-        setDuration((current) => { return { from: sub(current.from, { days: 7 }), to: sub(current.to, { days: 7 }) } });
+        setDuration((current) => { return { from: subDays(current.from, 7), to: subDays(current.to, 7) } });
     };
 
     const nextWeek = () => {
@@ -49,38 +46,16 @@ export const WeeklyReport: React.FC = () => {
     }
 
     return (
-        <S.ReportRoot>
-            <S.CardRoot>
-                <Button onClick={prevWeek} style={{minWidth: 40, height: 48, alignSelf: "center"}}><TiChevronLeft size={28} color="#546e7a"/></Button>
-                <S.Card>
-                    <S.CardHeader>
-                        <div>
-                            <AiOutlineCheckCircle />
-                            <span style={{fontSize: 20, paddingLeft: 2}}>Kakeiboooo</span>
-                        </div>
-                        <div style={{paddingRight: 12}}><GiPayMoney size={28}/></div>
-                    </S.CardHeader>
-                    <S.CardBody>
-                        <div style={{display: 'flex', flexDirection: 'column'}}>
-                            <span style={{ fontSize: 12, fontWeight: 500, color: "#BDBDBD" }}><u>出費</u></span>
-                            <span style={{ fontSize: 28, fontWeight: 700, paddingLeft: 4 }}>¥{getWeeklyTotalCost().toLocaleString()}</span>
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                            <span style={{ fontSize: 12, fontWeight: 500, color: "#BDBDBD" }}><u>期間</u></span>
-                            <span style={{ paddingLeft: 4, fontSize: 16}}>{duration.from.toLocaleDateString()} - {duration.to.toLocaleDateString()}</span>
-                        </div>
-                    </S.CardBody>
-                </S.Card>
-                <Button onClick={nextWeek} style={{ minWidth: 40, height: 48, alignSelf: "center" }}><TiChevronRight size={28} color="#546e7a" /></Button>
-            </S.CardRoot>
-            <WeeklyCosting duration={duration} cost={weeklyReceipt} />
+        <S.Root>
+            <WeeklyCostCard duration={duration} totalCost={getWeeklyTotalCost()} onClickPrev={prevWeek} onClickNext={nextWeek}/>
+            <WeeklyTransition duration={duration} weeklyCost={weeklyReceipt} />
             <MemoEditor/>
-        </S.ReportRoot>
+        </S.Root>
     )
 };
 
 const S = {
-    ReportRoot: styled.div`
+    Root: styled.div`
         width: 30%;
         height: 100%;
         padding: 0 0 0 12px;
@@ -88,33 +63,4 @@ const S = {
         flex-direction: column;
         justify-content: space-evenly;
     `,
-    CardRoot: styled.div`
-        display: flex;
-        align-items: stretch;
-        justify-content: space-between;
-        height: 25%;
-        width: 100%;
-    `,
-    Card: styled.div`
-        background: #546e7a;
-        width: 100%;
-        border-radius: 8px;
-        padding: 12px;
-        color: #FFFFFF;
-        font-weight: 700;
-    `,
-    CardHeader: styled.div`
-        height: 20%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        /* font-family: 'Bungee Hairline', cursive; */
-    `,
-    CardBody: styled.div`
-        height: 80%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        /* font-family: 'Bungee Hairline', cursive; */
-    `
 }
