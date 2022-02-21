5b560805b58538b3e7b369556e70ccbf2654a0bc
konno-yu <yuta.konno.socie@gmail.com>
2022-02-21 23:52:59 +0900

    家計簿まわりの不要なState、Actionを消しておく

--- src/reducer/householdBookSlice.ts
+++ src/reducer/householdBookSlice.ts
@@ -1,41 +1,26 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { getMonth, getYear, setDate, setMonth } from "date-fns";
-import MonthlyReceiptModel from "../components/receipt/model/MonthlyReceiptModel";
+import { setDate } from "date-fns";
 
 interface HouseholdBookState {
     // 表示対象の年月日
     targetDate: Date;
-    // 表示対象月における日別の食費
-    monthlyReceipt: MonthlyReceiptModel;
 }
 
 const initialState: HouseholdBookState = {
-    targetDate: new Date(),
-    monthlyReceipt: new MonthlyReceiptModel(new Date(), undefined),
+    // TODO 暫定（ほんとうは今日の日付にしておくべき）
+    targetDate: new Date(2022, 0, 31),
 }
 
 export const householdBookSlice = createSlice({
     name: 'household_book',
     initialState,
     reducers: {
-        showNextMonth: (state: HouseholdBookState) => {
-            state.targetDate = setMonth(state.targetDate, getMonth(state.targetDate) + 1)
-        },
-        showPrevMonth: (state: HouseholdBookState) => {
-            state.targetDate = setMonth(state.targetDate, getMonth(state.targetDate) - 1)
-        },
-        showSpecifyDate: (state: HouseholdBookState, action: PayloadAction<number>) => {
+        selectEdittingDate: (state: HouseholdBookState, action: PayloadAction<number>) => {
             state.targetDate = setDate(state.targetDate, action.payload);
-        },
-        updateMonthlyReceipt: (state: HouseholdBookState, action: PayloadAction<MonthlyReceiptModel>) => {
-            state.monthlyReceipt = action.payload;
         },
     }
 })
 
 export const {
-    showNextMonth,
-    showPrevMonth,
-    showSpecifyDate,
-    updateMonthlyReceipt,
+    selectEdittingDate,
 } = householdBookSlice.actions;
