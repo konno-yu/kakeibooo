8d17bd0281ee197cbc9bbb70872616aaeb8f3d33
konno-yu <yuta.konno.socie@gmail.com>
2022-02-23 00:32:27 +0900

    ボタンのコンポーネントストーリーの書き方を修正

---
+++ src/components/common/button/Button.stories.tsx
@@ -0,0 +1,41 @@
+import { ComponentStory, ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { FaSmileWink } from 'react-icons/fa';
+import { Button } from './Button';
+
+export default { component: Button } as ComponentMeta<typeof Button>;
+
+export const Filled: ComponentStoryObj<typeof Button> = {
+  args: {
+    label: 'これはボタンです',
+    variant: 'filled',
+    color: 'normal',
+    width: 200
+  }
+};
+
+export const Outlined: ComponentStoryObj<typeof Button> = {
+  args: {
+    label: 'これはボタンです',
+    variant: 'outlined',
+    color: 'accent',
+    width: 200
+  }
+};
+
+export const Icon: ComponentStoryObj<typeof Button> = {
+  args: {
+    label: 'これはボタンです',
+    variant: 'filled',
+    color: 'normal',
+    icon: <FaSmileWink />,
+    width: 200
+  }
+};
+
+export const Disabled: ComponentStoryObj<typeof Button> = {
+  args: {
+    label: 'これはボタンです',
+    disabled: true,
+    width: 200
+  }
+};
