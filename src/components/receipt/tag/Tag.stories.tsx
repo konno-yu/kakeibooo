5074e7842afa88a9fcc3bd4adfc1aeaad7c69d64
konno-yu <yuta.konno.socie@gmail.com>
2022-02-23 15:21:57 +0900

    タグ部分もRedux対応が必要

--- src/components/receipt/tag/Tag.stories.tsx
+++ src/components/receipt/tag/Tag.stories.tsx
@@ -1,9 +1,11 @@
 import { ComponentMeta, ComponentStory, ComponentStoryObj } from "@storybook/react";
+import { Provider } from "react-redux";
+import { store } from "../../../store";
 import { Tag } from "./Tag";
 
 export default { component: Tag } as ComponentMeta<typeof Tag>;
 
 export const Pure: ComponentStoryObj<typeof Tag> = {
     args: {},
-    decorators: [story => <div style={{ width: '25%' }}>{story()}</div>]
+    decorators: [story => <Provider store={store}><div style={{ width: '25%' }}>{story()}</div></Provider>]
 };
