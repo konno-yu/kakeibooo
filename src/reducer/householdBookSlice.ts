5b9cd5f2fa1cb8a7a04d0c159948eb33bd54b4c4
konno-yu <yuta.konno.socie@gmail.com>
2022-02-23 13:23:40 +0900

    レシートのタグを扱うためのStateとActionを追加

--- src/reducer/householdBookSlice.ts
+++ src/reducer/householdBookSlice.ts
@@ -4,11 +4,13 @@ import { setDate } from "date-fns";
 interface HouseholdBookState {
     // 表示対象の年月日
     targetDate: Date;
+    tags: { index: number, storeName: string, cost: number }[]
 }
 
 const initialState: HouseholdBookState = {
     // TODO 暫定（ほんとうは今日の日付にしておくべき）
     targetDate: new Date(2022, 0, 31),
+    tags: []
 }
 
 export const householdBookSlice = createSlice({
@@ -18,9 +20,29 @@ export const householdBookSlice = createSlice({
         selectEdittingDate: (state: HouseholdBookState, action: PayloadAction<number>) => {
             state.targetDate = setDate(state.targetDate, action.payload);
         },
+        addTag: (state: HouseholdBookState) => {
+            state.tags.push({ index: state.tags.length, storeName: '', cost: null });
+        },
+        deleteTag: (state: HouseholdBookState, action: PayloadAction<number>) => {
+            state.tags = state.tags.filter(tag => tag.index !== action.payload);
+            state.tags = state.tags.map((tag, i) => {
+                return { index: i, ...tag };
+            });
+        },
+        inputStoreName: (state: HouseholdBookState, action: PayloadAction<{ index: number, storeName: string }>) => {
+            console.log(action.payload);
+            state.tags[action.payload.index].storeName = action.payload.storeName;
+        },
+        inputCost: (state: HouseholdBookState, action: PayloadAction<{ index: number, cost: number }>) => {
+            state.tags[action.payload.index].cost = action.payload.cost;
+        }
     }
 })
 
 export const {
     selectEdittingDate,
+    addTag,
+    deleteTag,
+    inputStoreName,
+    inputCost,
 } = householdBookSlice.actions;
