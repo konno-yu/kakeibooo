c184be6be21723302f8cb42bb01e28339e3563e8
konno-yu <yuta.konno.socie@gmail.com>
2022-03-05 13:00:49 +0900

    レシートの表示を前提にデータの持ち方を修正
    総額だけでなく店舗名と金額をもつようにする

--- src/components/calendar/Calendar.stories.tsx
+++ src/components/calendar/Calendar.stories.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 import { Provider } from 'react-redux';
 import { store } from '../../store';
@@ -9,46 +10,46 @@ export const Pure: ComponentStoryObj<typeof Calendar> = {
   args: {
     today: new Date(2022, 0, 1),
     datas: {
-      1: [null, null, null, null, null, null, { date: new Date(2022, 0, 1), totalCost: 0 }],
+      1: [null, null, null, null, null, null, { date: new Date(2022, 0, 1), receipts: [] }],
       2: [
-        { date: new Date(2022, 0, 2), totalCost: 300 },
-        { date: new Date(2022, 0, 3), totalCost: 600 },
-        { date: new Date(2022, 0, 4), totalCost: 900 },
-        { date: new Date(2022, 0, 5), totalCost: 1200 },
-        { date: new Date(2022, 0, 6), totalCost: 1500 },
-        { date: new Date(2022, 0, 7), totalCost: 1800 },
-        { date: new Date(2022, 0, 8), totalCost: 2100 },
+        { date: new Date(2022, 0, 2), receipts: [{ storeName: 'sample', cost: 300 }] },
+        { date: new Date(2022, 0, 3), receipts: [{ storeName: 'sample', cost: 600 }] },
+        { date: new Date(2022, 0, 4), receipts: [{ storeName: 'sample', cost: 900 }] },
+        { date: new Date(2022, 0, 5), receipts: [{ storeName: 'sample', cost: 1200 }] },
+        { date: new Date(2022, 0, 6), receipts: [{ storeName: 'sample', cost: 1500 }] },
+        { date: new Date(2022, 0, 7), receipts: [{ storeName: 'sample', cost: 1800 }] },
+        { date: new Date(2022, 0, 8), receipts: [{ storeName: 'sample', cost: 2100 }] },
       ],
       3: [
-        { date: new Date(2022, 0, 9), totalCost: 2400 },
-        { date: new Date(2022, 0, 10), totalCost: 2700 },
-        { date: new Date(2022, 0, 11), totalCost: 3000 },
-        { date: new Date(2022, 0, 12), totalCost: 2700 },
-        { date: new Date(2022, 0, 13), totalCost: 2400 },
-        { date: new Date(2022, 0, 14), totalCost: 2100 },
-        { date: new Date(2022, 0, 15), totalCost: 1800 },
+        { date: new Date(2022, 0, 9), receipts: [{ storeName: 'sample', cost: 2700 }] },
+        { date: new Date(2022, 0, 10), receipts: [{ storeName: 'sample', cost: 3000 }] },
+        { date: new Date(2022, 0, 11), receipts: [{ storeName: 'sample', cost: 2700 }] },
+        { date: new Date(2022, 0, 12), receipts: [{ storeName: 'sample', cost: 2100 }] },
+        { date: new Date(2022, 0, 13), receipts: [{ storeName: 'sample', cost: 1800 }] },
+        { date: new Date(2022, 0, 14), receipts: [{ storeName: 'sample', cost: 1500 }] },
+        { date: new Date(2022, 0, 15), receipts: [{ storeName: 'sample', cost: 1200 }] },
       ],
       4: [
-        { date: new Date(2022, 0, 16), totalCost: 1500 },
-        { date: new Date(2022, 0, 17), totalCost: 1200 },
-        { date: new Date(2022, 0, 18), totalCost: 900 },
-        { date: new Date(2022, 0, 19), totalCost: 600 },
-        { date: new Date(2022, 0, 20), totalCost: 300 },
-        { date: new Date(2022, 0, 21), totalCost: 0 },
-        { date: new Date(2022, 0, 22), totalCost: 300 },
+        { date: new Date(2022, 0, 16), receipts: [{ storeName: 'sample', cost: 900 }] },
+        { date: new Date(2022, 0, 17), receipts: [{ storeName: 'sample', cost: 600 }] },
+        { date: new Date(2022, 0, 18), receipts: [{ storeName: 'sample', cost: 300 }] },
+        { date: new Date(2022, 0, 19), receipts: [{ storeName: 'sample', cost: 600 }] },
+        { date: new Date(2022, 0, 20), receipts: [{ storeName: 'sample', cost: 900 }] },
+        { date: new Date(2022, 0, 21), receipts: [] },
+        { date: new Date(2022, 0, 22), receipts: [{ storeName: 'sample', cost: 1200 }] },
       ],
       5: [
-        { date: new Date(2022, 0, 23), totalCost: 600 },
-        { date: new Date(2022, 0, 24), totalCost: 900 },
-        { date: new Date(2022, 0, 25), totalCost: 1200 },
-        { date: new Date(2022, 0, 26), totalCost: 1500 },
-        { date: new Date(2022, 0, 27), totalCost: 1800 },
-        { date: new Date(2022, 0, 28), totalCost: 2100 },
-        { date: new Date(2022, 0, 29), totalCost: 2400 },
+        { date: new Date(2022, 0, 23), receipts: [{ storeName: 'sample', cost: 1500 }] },
+        { date: new Date(2022, 0, 24), receipts: [{ storeName: 'sample', cost: 1800 }] },
+        { date: new Date(2022, 0, 25), receipts: [{ storeName: 'sample', cost: 2100 }] },
+        { date: new Date(2022, 0, 26), receipts: [{ storeName: 'sample', cost: 2400 }] },
+        { date: new Date(2022, 0, 27), receipts: [{ storeName: 'sample', cost: 2700 }] },
+        { date: new Date(2022, 0, 28), receipts: [{ storeName: 'sample', cost: 3000 }] },
+        { date: new Date(2022, 0, 29), receipts: null },
       ],
       6: [
-        { date: new Date(2022, 0, 30), totalCost: 2700 },
-        { date: new Date(2022, 0, 31), totalCost: 3000 },
+        { date: new Date(2022, 0, 30), receipts: null },
+        { date: new Date(2022, 0, 31), receipts: null },
         null,
         null,
         null,
@@ -60,8 +61,14 @@ export const Pure: ComponentStoryObj<typeof Calendar> = {
   decorators: [
     (story) => (
       <Provider store={store}>
-        <div style={{ padding: 8, width: '75%', background: '#ECEFF1' }}>{story()}</div>
+        <div css={decorator}>{story()}</div>
       </Provider>
     ),
   ],
 };
+
+const decorator = css`
+  width: 75%;
+  background: ECEFF1;
+  padding: 8px;
+`;
--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -7,7 +7,9 @@ import { DayPanel } from './day_panel/DayPanel';
 import { Header } from './header/Header';
 
 interface Props {
-  datas: { [key in number]: { date: Date; totalCost: number }[] };
+  datas: {
+    [key: number]: (null | { date: Date; receipts: { storeName: string; cost: number }[] | [] | null })[];
+  };
   today: Date;
 }
 
@@ -17,6 +19,13 @@ export const Calendar: React.FC<Props> = ({ datas, today }: Props) => {
     if (cost <= 1000) return 'low';
     if (cost > 2500) return 'high';
     return 'normal';
+  };
+
+  const calculateTotalCost = (receipts: { storeName: string; cost: number }[]) => {
+    let totalCost = 0;
+    // eslint-disable-next-line no-return-assign
+    receipts.forEach((r) => (totalCost += r.cost));
+    return totalCost;
   };
   const dispatch = useAppDispatch();
   const targetDate = useAppSelector((state) => state.householdBook.targetDate);
@@ -39,15 +48,29 @@ export const Calendar: React.FC<Props> = ({ datas, today }: Props) => {
               if (day === null) {
                 return <DayPanel type="normal" dayIndex={null} isSelected={false} isToday={false} />;
               }
+              if (day.receipts === null) {
+                return (
+                  <DayPanel
+                    type="normal"
+                    dayIndex={getDate(day.date)}
+                    isSelected={isEqual(day.date, targetDate)}
+                    isToday={isEqual(day.date, today)}
+                    onClick={handleOnClick}
+                  >
+                    {null}
+                  </DayPanel>
+                );
+              }
+              const totalCost = day.receipts.length === 0 ? 0 : calculateTotalCost(day.receipts);
               return (
                 <DayPanel
-                  type={getPanelType(day.totalCost)}
+                  type={getPanelType(totalCost)}
                   dayIndex={getDate(day.date)}
                   isSelected={isEqual(day.date, targetDate)}
                   isToday={isEqual(day.date, today)}
                   onClick={handleOnClick}
                 >
-                  {day.totalCost}
+                  {totalCost}
                 </DayPanel>
               );
             })}
@@ -60,7 +83,7 @@ export const Calendar: React.FC<Props> = ({ datas, today }: Props) => {
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   gap: 8px;
