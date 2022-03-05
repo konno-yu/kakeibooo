297767f4f8da2f39273e85a644179ff106e59e5e
konno-yu <yuta.konno.socie@gmail.com>
2022-03-05 09:57:15 +0900

    emotion導入
    babel-loaderはほんとうにひつよう？

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: [{ loader: 'ts-loader' }],
         exclude: /node_modules/,
       },
       {
