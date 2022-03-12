95eb7edb98f7642b5e9b90c4992b119d0907425d
konno-yu <yuta.konno.socie@gmail.com>
2022-03-12 10:15:39 +0900

    家計簿画面のSliceファイルを更新
    ・レシートの管理はここではやらない
    ・月の移動を扱うActionを追加
    ・指定された年月のテンプレート（何曜始まりか、何週あるかなど）を作成する処理を追加

--- src/reducer/householdBookSlice.ts
+++ src/reducer/householdBookSlice.ts
@@ -1,39 +1,86 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { setDate } from 'date-fns';
+import { endOfMonth, getDate, getDay, getMonth, getWeekOfMonth, getYear, setDate, setMonth } from 'date-fns';
 
 interface HouseholdBookState {
-  // 表示対象の年月日
+  /** 編集対象の年月日 */
   targetDate: Date;
-  tags: { index: number; storeName: string; cost: number }[];
+  /** 表示中の月におけるレシート */
+  receipts: {
+    [key: number]: (null | {
+      date: Date;
+      receipts: { index: number; storeName: string; cost: number }[] | [] | null;
+    })[];
+  };
 }
 
+/**
+ * 指定された年月のテンプレートをつくる
+ */
+export const createMonthTemplate = (
+  targetDate: Date
+): {
+  [key: number]: (null | { date: Date; receipts: { index: number; storeName: string; cost: number }[] | [] | null })[];
+} => {
+  const template: {
+    [key: number]: (null | {
+      date: Date;
+      receipts: { index: number; storeName: string; cost: number }[] | [] | null;
+    })[];
+  } = {
+    1: [null, null, null, null, null, null, null],
+    2: [null, null, null, null, null, null, null],
+    3: [null, null, null, null, null, null, null],
+    4: [null, null, null, null, null, null, null],
+    5: [null, null, null, null, null, null, null],
+    6: [null, null, null, null, null, null, null],
+  };
+  const lastDayOfMonth = getDate(endOfMonth(targetDate));
+  for (let day = 1; day <= lastDayOfMonth; day += 1) {
+    const date = new Date(getYear(targetDate), getMonth(targetDate), day);
+    const weekIndex = getWeekOfMonth(date);
+    const dayIndex = getDay(date);
+
+    /** ほんとうはこっち */
+    // template[weekIndex][dayIndex] = { date, receipts: null };
+    if (dayIndex === 0) {
+      template[weekIndex][dayIndex] = { date, receipts: [] };
+    } else {
+      template[weekIndex][dayIndex] = {
+        date,
+        receipts: [
+          { index: 0, storeName: `サンプル${dayIndex}`, cost: 1000 * dayIndex },
+          { index: 1, storeName: `サンプル${dayIndex}`, cost: 1000 * dayIndex },
+        ],
+      };
+    }
+  }
+  return template;
+};
+
 const initialState: HouseholdBookState = {
-  // TODO 暫定（ほんとうは今日の日付にしておくべき）
-  targetDate: new Date(2022, 0, 31),
-  tags: [],
+  targetDate: new Date(new Date().setHours(0, 0, 0, 0)),
+  receipts: createMonthTemplate(new Date()),
 };
 
 export const householdBookSlice = createSlice({
   name: 'household_book',
   initialState,
   reducers: {
+    /** 編集日を選択する */
     selectEdittingDate: (state: HouseholdBookState, action: PayloadAction<number>) => {
       state.targetDate = setDate(state.targetDate, action.payload);
     },
-    addTag: (state: HouseholdBookState) => {
-      state.tags.push({ index: state.tags.length, storeName: '', cost: null });
+    /** 前月を表示する */
+    shiftPreviousMonth: (state: HouseholdBookState) => {
+      state.targetDate = setMonth(state.targetDate, getMonth(state.targetDate) - 1);
+      state.receipts = createMonthTemplate(state.targetDate);
     },
-    deleteTag: (state: HouseholdBookState, action: PayloadAction<number>) => {
-      state.tags = state.tags.filter((tag) => tag.index !== action.payload);
-      state.tags = state.tags.map((tag, i) => ({ index: i, ...tag }));
-    },
-    inputStoreName: (state: HouseholdBookState, action: PayloadAction<{ index: number; storeName: string }>) => {
-      state.tags[action.payload.index].storeName = action.payload.storeName;
-    },
-    inputCost: (state: HouseholdBookState, action: PayloadAction<{ index: number; cost: number }>) => {
-      state.tags[action.payload.index].cost = action.payload.cost;
+    /** 来月を表示する */
+    shiftNextMonth: (state: HouseholdBookState) => {
+      state.targetDate = setMonth(state.targetDate, getMonth(state.targetDate) + 1);
+      state.receipts = createMonthTemplate(state.targetDate);
     },
   },
 });
 
-export const { selectEdittingDate, addTag, deleteTag, inputStoreName, inputCost } = householdBookSlice.actions;
+export const { selectEdittingDate, shiftPreviousMonth, shiftNextMonth } = householdBookSlice.actions;
