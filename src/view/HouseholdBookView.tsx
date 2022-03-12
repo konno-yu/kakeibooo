9d8fc9b080099143fa08889d38e46b5528c528eb
konno-yu <yuta.konno.socie@gmail.com>
2022-03-12 10:18:15 +0900

    家計簿画面の親となるコンポーネントを微修正
    emotionに書き換えなど

--- src/view/HouseholdBookView.tsx
+++ src/view/HouseholdBookView.tsx
@@ -1,20 +1,60 @@
-// import * as React from 'react';
-// import styled from 'styled-components';
-// import { Calendar } from '../components/calendar/__Calendar';
-// import { Receipt } from '../components/receipt/Receipt';
+import * as React from 'react';
+import { css } from '@emotion/react';
+import { getWeekOfMonth, isEqual } from 'date-fns';
+import { Calendar } from '../components/calendar/Calendar';
+import { Receipt } from '../components/receipt/Receipt';
+import { useAppSelector } from '../store';
 
-// export const HouseholdBookView: React.FC = () => (
-//   <SC.FinanceView>
-//     <Calendar />
-//     <Receipt />
-//   </SC.FinanceView>
-// );
+/**
+ * 特定の日のレシートだけ抽出する
+ * TODO ここにいてよいのかは謎、Sliceに移すべきかもしれない
+ */
+export const extractTargetDayReceipt = (
+  receipts: {
+    [key: number]: (null | {
+      date: Date;
+      receipts: { index: number; storeName: string; cost: number }[] | [] | null;
+    })[];
+  },
+  targetDate: Date
+) => {
+  const weeklyReceipt = receipts[getWeekOfMonth(targetDate)].filter((receipt) => receipt);
+  const dailyReceipt = weeklyReceipt.filter((wr) => wr.date && isEqual(wr.date, targetDate))[0].receipts;
+  return dailyReceipt;
+};
 
-// const SC = {
-//   FinanceView: styled.div`
-//     height: calc(100vh - 24px);
-//     padding: 12px;
-//     display: flex;
-//     justify-content: space-between;
-//   `,
-// };
+export const HouseholdBookView: React.FC = () => {
+  const receipts = useAppSelector((state) => state.householdBook.receipts);
+  const targetDate = useAppSelector((state) => state.householdBook.targetDate);
+  const today = new Date(new Date().setHours(0, 0, 0, 0));
+
+  const getTags = () => extractTargetDayReceipt(receipts, targetDate);
+
+  return (
+    <div css={householdBookContainer}>
+      <div css={calendarContainer}>
+        <Calendar today={today} datas={receipts} />
+      </div>
+      <div css={receiptContainer}>
+        <Receipt tags={getTags()} />
+      </div>
+    </div>
+  );
+};
+
+const householdBookContainer = css`
+  height: calc(100vh - 24px);
+  width: calc(100% - 24px);
+  padding: 12px;
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+`;
+
+const calendarContainer = css`
+  width: 75%;
+`;
+
+const receiptContainer = css`
+  width: 25%;
+`;
