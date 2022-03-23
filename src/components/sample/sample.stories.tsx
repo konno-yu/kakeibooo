324ecf1e45d37569703d64afa001382683ad796c
konno-yu <yuta.konno.socie@gmail.com>
2022-03-24 00:05:45 +0900

    サンプル実装を削除

--- src/components/sample/sample.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-import { Sample } from './sample';
-
-export default { component: Sample } as ComponentMeta<typeof Sample>;
-
-export const Pure: ComponentStoryObj<typeof Sample> = {
-  args: {},
-};
