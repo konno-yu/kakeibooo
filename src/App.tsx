fb569a580e1165fcd1bb39b1797eefd63b2201d1
konno-yu <yuta.konno.socie@gmail.com>
2022-04-04 00:18:12 +0900

    React Router DOM によるルーティングを仮実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,58 @@
-import { StylesProvider } from '@material-ui/styles';
+import { css } from '@emotion/react';
 import * as React from 'react';
+import { RiEmotionSadLine } from 'react-icons/ri';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import styled from 'styled-components';
-import { Drawer } from './components/drawer/Drawer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PrivateRoute } from './routes/PrivateRoute';
+import { RootRoute } from './routes/RootRoute';
+// eslint-disable-next-line import/no-unresolved
+import { AuthListener } from './services/authListener';
 import { store } from './store';
-// import { HomeView } from './view/HomeView';
 import { HouseholdBookView } from './view/HouseholdBookView';
 import { LoginView } from './view/LoginView';
 
 export const App: React.FC = () => (
   <Provider store={store}>
-    <LoginView />
-    {/* <SC.Drawer>
-      <Drawer />
-    </SC.Drawer>
-    <SC.Content>
-      <HouseholdBookView />
-    </SC.Content> */}
+    <AuthListener>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootRoute />} />
+          <Route path="/signin" element={<LoginView />} />
+          <Route
+            path="/householdbook"
+            element={
+              <PrivateRoute>
+                <HouseholdBookView />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthListener>
   </Provider>
 );
 
-const SC = {
-  Drawer: styled.div`
-    font-family: 'M PLUS Rounded 1c', sans-serif;
-    height: 100vh;
-    width: 15%;
-    border-right: 1px solid #eeeeee;
-  `,
-  Content: styled.div`
-    width: 85%;
-    height: 100vh;
-    background: #eceff1;
-  `,
-};
+// TODO 暫定
+const NotFound = () => (
+  <div css={notFound}>
+    <h1>404</h1>
+    <h3>お探しのページは見つかりませんでした。</h3>
+  </div>
+);
+
+const UnderConstruction = () => (
+  <div css={notFound}>
+    <RiEmotionSadLine color="#009688" size={48} />
+    <h3>鋭意製作中です</h3>
+  </div>
+);
+
+const notFound = css`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
