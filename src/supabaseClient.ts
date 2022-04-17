23e929597a9758c22364039e0fcbdda9e4b106e3
konno-yu <yuta.konno.socie@gmail.com>
2022-04-17 09:03:56 +0900

    Vercelデプロイテスト

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
