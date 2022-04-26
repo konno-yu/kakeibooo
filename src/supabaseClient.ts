41fc99e74d118295c8ff2ba21b9d8b6809b3cfb4
konno-yu <yuta.konno.socie@gmail.com>
2022-04-26 23:50:33 +0900

    コメントアウトしていたところを元に戻す

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-// console.log(supabaseUrl);
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+console.log(supabaseUrl);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
