b9a24b027f6db9074d59b014dd7bab80e883d030
konno-yu <yuta.konno.socie@gmail.com>
2022-04-29 15:26:01 +0900

    確認用出力

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,4 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+console.log(supabaseUrl);
+console.log(supabaseAnonKey);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
