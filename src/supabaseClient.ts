da86de67d22620d35bb6d2403c054175f5fa7b7e
konno-yu <yuta.konno.socie@gmail.com>
2022-04-29 15:11:34 +0900

    確認用のコンソール出力は消しておく

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-console.log(supabaseUrl);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
