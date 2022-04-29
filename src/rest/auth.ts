141c707fe0c9682533715c3530deaed52152bf14
konno-yu <yuta.konno.socie@gmail.com>
2022-04-29 15:10:43 +0900

    コメントアウトしていたところを元に戻す

--- src/rest/auth.ts
+++ src/rest/auth.ts
@@ -1,11 +1,11 @@
-// import { supabase } from '../supabaseClient';
+import { supabase } from '../supabaseClient';
 
 export const signIn = async (obj: { email: string; password: string }) => {
-  // const { error, session } = await supabase.auth.signIn(obj);
-  // return { error, session };
+  const { error, session } = await supabase.auth.signIn(obj);
+  return { error, session };
 };
 
 export const signOut = async () => {
-  // const { error } = await supabase.auth.signOut();
-  // return { error };
+  const { error } = await supabase.auth.signOut();
+  return { error };
 };
