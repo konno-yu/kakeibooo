8ac0c05c8fb6368bece2ca54a121f957437bbc76
konno-yu <konno-yu@users.noreply.github.com>
2022-05-09 15:37:03 +0000

    Deploying to develop from @ konno-yu/kakeibooo@57129bbd5366568288f3946c961cb8fb4fa4f64e 🚀

--- src/components/home/week_summary/WeekSummary.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-import { WeekSummary } from './WeekSummary';
-
-export default { component: WeekSummary } as ComponentMeta<typeof WeekSummary>;
-
-export const Pure: ComponentStoryObj<typeof WeekSummary> = {
-  args: {},
-  decorators: [(story) => <div style={{ width: '25%' }}>{story()}</div>],
-};
