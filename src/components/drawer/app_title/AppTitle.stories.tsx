f1d04b76e96a33d8710af81d7312d3ca674cb04d
konno-yu <konno-yu@users.noreply.github.com>
2022-05-23 14:48:35 +0000

    Deploying to develop from  @ 8caf4b236bdfea5dd4cc95b101769eaf2509fce1 🚀

--- src/components/drawer/app_title/AppTitle.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-import { AppTitle } from './AppTitle';
-
-export default { component: AppTitle } as ComponentMeta<typeof AppTitle>;
-
-export const Pure: ComponentStoryObj<typeof AppTitle> = {
-  args: {},
-};
