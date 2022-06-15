1e996a5d9ff87d5c8fa276fe32a41f9c83636c79
konno-yu <yuta.konno.socie@gmail.com>
2022-06-16 00:07:09 +0900

    メッセージリソースはtsファイルで管理する
    ・画面ごとなどで階層を分けたい
    ・コメントを書きたい

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,10 @@
-import { setupWorker } from 'msw';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from '../src/store';
-import { initialize, mswDecorator } from 'msw-storybook-addon';
+import { mswDecorator } from 'msw-storybook-addon';
 import { ThemeProvider } from '@emotion/react';
 import { light } from '../src/theme/theme';
-import { i18n } from './i18next.js';
+import { i18n } from './i18n';
 
 if (typeof global.process === 'undefined') {
   const { worker } = require('../src/mocks/browser');
