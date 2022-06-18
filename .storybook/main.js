d8290390aa78921f8bc08442bdc276d866e5d209
konno-yu <yuta.konno.socie@gmail.com>
2022-06-18 09:36:51 +0900

    テスト環境の整備①
    ・Jestでテストが書けるようにする
    ・ストーリーファイルに書いたplay関数の動きをStorybook上で確認できるようにする

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,19 @@ module.exports = {
     builder: 'webpack5',
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-react-i18next'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-react-i18next',
+    '@storybook/addon-interactions',
+  ],
   staticDirs: ['./public'],
   babel: async (options) => ({
     ...options,
     presets: [...options.presets, '@emotion/babel-preset-css-prop'],
   }),
+  features: {
+    interactionsDebugger: true,
+  },
   framework: '@storybook/react',
 };
