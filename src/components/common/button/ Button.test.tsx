3b609e067c3f85f22342f1d6d93820d1e537d612
konno-yu <yuta.konno.socie@gmail.com>
2022-06-20 00:21:31 +0900

    Button, IconButtonコンポーネントのテストを追加

---
+++ src/components/common/button/ Button.test.tsx
@@ -0,0 +1,23 @@
+/** @jest-environment jsdom */
+import { composeStories } from '@storybook/testing-react';
+import * as ButtonStories from './Button.stories';
+import { render } from '../../../test-utils';
+
+const { ClickFilled, ClickDisabled } = composeStories(ButtonStories);
+
+describe('Buttonコンポーネント', () => {
+  it('クリックしたらonClickが呼ばれる', async () => {
+    const mockOnClick = jest.fn();
+    const { container } = render(<ClickFilled onClick={mockOnClick} />);
+    await ClickFilled.play({ canvasElement: container });
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
+  it('非活性の場合クリックしてもonClickは呼ばれない', async () => {
+    const mockOnClick = jest.fn();
+    const { container } = render(<ClickDisabled onClick={mockOnClick} />);
+    await ClickDisabled.play({ canvasElement: container });
+    expect(mockOnClick).toHaveBeenCalledTimes(0);
+  });
+});
+
+// TODO アクセシビリティ対応
