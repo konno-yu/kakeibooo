70119a2bafd1089c7fb052bb985b15052675c516
konno-yu <yuta.konno.socie@gmail.com>
2022-06-22 00:23:41 +0900

    react-18nextを使ったコンポーネントでwarningが表示されないようにする

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from '@emotion/react';
 import { render, RenderOptions } from '@testing-library/react';
 import React, { ReactElement } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import { light } from './theme/theme';
 
 const AllTheProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <ThemeProvider theme={light}>{children}</ThemeProvider>
+  <ThemeProvider theme={light}>
+    <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+  </ThemeProvider>
 );
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
