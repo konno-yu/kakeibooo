b3fa96657c7c776e87dd3fc72ff798dc3561c3fc
konno-yu <yuta.konno.socie@gmail.com>
2022-07-06 23:45:39 +0900

    Memoコンポーネントは一旦コメントアウト
    Quillは使わずLexicalを使うようにしたいところ

--- src/components/home/week_memo_editor/WeekMemoEditor.stories.tsx
+++ src/components/home/week_memo_editor/WeekMemoEditor.stories.tsx
@@ -1,9 +1,9 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-import { WeekMemoEditor } from './WeekMemoEditor';
+// import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+// import { WeekMemoEditor } from './WeekMemoEditor';
 
-export default { component: WeekMemoEditor } as ComponentMeta<typeof WeekMemoEditor>;
+// export default { component: WeekMemoEditor } as ComponentMeta<typeof WeekMemoEditor>;
 
-export const Pure: ComponentStoryObj<typeof WeekMemoEditor> = {
-  args: {},
-  decorators: [(story) => <div style={{ width: '30%' }}>{story()}</div>],
-};
+// export const Pure: ComponentStoryObj<typeof WeekMemoEditor> = {
+//   args: {},
+//   decorators: [(story) => <div style={{ width: '30%' }}>{story()}</div>],
+// };
