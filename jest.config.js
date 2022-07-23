66ac622e0417f82e2c1845a7cde613873382919a
konno-yu <yuta.konno.socie@gmail.com>
2022-07-23 14:06:11 +0900

    swcを入れてテスト高速化

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   roots: ['./src'],
+  testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      '@swc/jest',
+      {
+        sourceMaps: true,
+        module: {
+          type: 'commonjs',
+        },
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
   },
 };
