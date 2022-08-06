b72a0b7067a527661714b456d645e29d68a523d9
konno-yu <yuta.konno.socie@gmail.com>
2022-08-06 16:33:21 +0900

    1週間の概況を表すWeekSummaryコンポーネントを実装
    昔作っていたコンポーネントを上書きする形で実装

--- src/components/home/week_summary/WeekSummary.stories.tsx
+++ src/components/home/week_summary/WeekSummary.stories.tsx
@@ -4,6 +4,8 @@ import { WeekSummary } from './WeekSummary';
 export default { component: WeekSummary } as ComponentMeta<typeof WeekSummary>;
 
 export const Pure: ComponentStoryObj<typeof WeekSummary> = {
-  args: {},
-  decorators: [(story) => <div style={{ width: '25%' }}>{story()}</div>],
+  args: {
+    dailyCost: [0, 500, 1000, 1500, 2000, 2500, 3000],
+  },
+  decorators: [(story) => <div style={{ width: '35%' }}>{story()}</div>],
 };
