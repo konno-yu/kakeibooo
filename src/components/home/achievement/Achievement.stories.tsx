c89f1d36d5f0893f68ce8a23dcf45188b7b0e5f5
konno-yu <yuta.konno.socie@gmail.com>
2022-09-12 18:37:07 +0900

    実績の達成状況を導出するための処理を実装

--- src/components/home/achievement/Achievement.stories.tsx
+++ src/components/home/achievement/Achievement.stories.tsx
@@ -1,13 +1,13 @@
 import { css } from '@emotion/react';
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 import { Achievement } from './Achievement';
-import { achievements } from './AchievementUtils';
+import { initAchievements } from './AchievementUtils';
 
 export default { component: Achievement } as ComponentMeta<typeof Achievement>;
 
 export const Pure: ComponentStoryObj<typeof Achievement> = {
   args: {
-    rewards: achievements,
+    rewards: initAchievements.map((a) => ({ ...a, isAchieve: true })),
   },
   decorators: [
     (story) => (
